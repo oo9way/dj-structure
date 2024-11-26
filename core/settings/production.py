944a81cd0e73d7ba6e774b352cc9f6767067f192
Muhammad Yusuf <mohammedyousef.dev@gmail.com>
2024-11-26 19:05:15 +0500

    allowed hosts

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', 'joseph.uz']
+ALLOWED_HOSTS = ['localhost', 'joseph.uz', 'academy.joseph.uz']
+CSRF_TRUSTED_ORIGINS = ['http://joseph.uz', 'https://joseph.uz']
